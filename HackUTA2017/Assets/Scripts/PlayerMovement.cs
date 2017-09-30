21ef15c90432d3efde6348377df7a3cb105bcdd8
SalvatoreCalderone <32420053+SalvatoreCalderone@users.noreply.github.com>
2017-09-30 13:26:24 -0500

    Add files via upload

---
+++ HackUTA2017/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerMovementSpeed;
+    public float jumpHeight;
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * playerMovementSpeed * Time.deltaTime, 0, 0));
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            transform.Translate(Vector2.up * jumpHeight * Time.deltaTime);
+    }
+}
