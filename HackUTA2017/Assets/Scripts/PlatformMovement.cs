44ea1368f1be0edda6084da69611840a2000921a
SalvatoreCalderone <32420053+SalvatoreCalderone@users.noreply.github.com>
2017-09-30 16:50:09 -0500

    Add files via upload

---
+++ HackUTA2017/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float moveDistX, moveDistY, moveSpeedX, moveSpeedY;
+    float startPositionX, startPositionY, maxPositionX, minPositionX, maxPositionY, minPositionY;
+    bool hasReachedTopX, hasReachedBottomX, hasReachedTopY, hasReachedBottomY;
+    private void Awake()
+    {
+        startPositionX = transform.position.x;
+        startPositionY = transform.position.y;
+        maxPositionX = startPositionX + moveDistX;
+        minPositionX = startPositionX - moveDistX;
+        maxPositionY = startPositionY + moveDistY;
+        minPositionY = startPositionY - moveDistY;
+        hasReachedBottomX = true;
+        hasReachedBottomY = true;
+        hasReachedTopX = false;
+        hasReachedTopY = false;
+        if (moveSpeedX <= 0)
+        {
+            hasReachedBottomX = false;
+            hasReachedTopX = false;
+        }
+        if (moveSpeedY <= 0)
+        {
+            hasReachedBottomY = false;
+            hasReachedTopY = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (hasReachedBottomX)
+        {
+            transform.Translate(Vector2.right * moveSpeedX * Time.deltaTime);
+            Debug.Log("Moving Right");
+            if (transform.position.x >= maxPositionX)
+            {
+                hasReachedBottomX = false;
+                hasReachedTopX = true;
+                Debug.Log("Reached The Right");
+            }
+        }
+        if (hasReachedTopX)
+        {
+            transform.Translate(Vector2.left * moveSpeedX * Time.deltaTime);
+            Debug.Log("Moving Left");
+            if (transform.position.x <= minPositionX)
+            {
+                hasReachedTopX = false;
+                hasReachedBottomX = true;
+                Debug.Log("Reached The Left");
+            }
+        }
+        if (hasReachedBottomY)
+        {
+            transform.Translate(Vector2.up * moveSpeedY * Time.deltaTime);
+            Debug.Log("Moving Up");
+            if (transform.position.y >= maxPositionY)
+            {
+                hasReachedTopY = true;
+                hasReachedBottomY = false;
+                Debug.Log("Reached The Top");
+            }
+        }
+        if (hasReachedTopY)
+        {
+            transform.Translate(Vector2.down * moveSpeedY * Time.deltaTime);
+            Debug.Log("Moving Down");
+            if (transform.position.y <= minPositionY)
+            {
+                hasReachedBottomY = true;
+                hasReachedTopY = false;
+                Debug.Log("Reached The Bottom");
+            }
+        }
+    }
+}
--- HackUTA2017/Assets/Scripts/PlayerMovement.cs
+++ HackUTA2017/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,31 @@ public class PlayerMovement : MonoBehaviour
     public float playerMovementSpeed;
     public float jumpHeight;
 
+    Rigidbody2D rigid;
+
+    bool hasJumped = false;
+
+    private void Awake()
+    {
+        rigid = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
         transform.Translate(new Vector3(Input.GetAxis("Horizontal") * playerMovementSpeed * Time.deltaTime, 0, 0));
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            transform.Translate(Vector2.up * jumpHeight * Time.deltaTime);
+       
+    }
+    private void FixedUpdate()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && !hasJumped)
+        {
+            rigid.AddForce(Vector2.up * jumpHeight * Time.fixedDeltaTime);
+            hasJumped = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            hasJumped = false;
     }
 }
